41b42b6acb9d77beccb88b4fcd80f535190debb7
Yasemin Bridges <y.bridges@qmul.ac.uk>
2024-05-22 10:24:14 +0100

    add TODO

--- src/pheval_oak/runner.py
+++ src/pheval_oak/runner.py
@@ -3,6 +3,8 @@ from pathlib import Path
 
 from pheval.runners.runner import PhEvalRunner
 
+from pheval_oak.prepare.prepare import prepare_inputs
+
 
 @dataclass
 class PhenologRunner(PhEvalRunner):
@@ -18,15 +20,18 @@ class PhenologRunner(PhEvalRunner):
         Pre-process any data and inputs necessary to run the tool.
         """
         print("preparing")
+        prepare_inputs(testdata_dir=self.testdata_dir)
 
     def run(self):
         """
         Run the tool to produce the raw output.
         """
-        print("running with fake predictor")
+        # TODO implement Best Matched Average & parse phenotype-gene for ranked list of genes
+        print("running with phenolog runner")
 
     def post_process(self):
         """
         Post-process the raw output into PhEval standardised TSV output.
         """
+        # TODO post process the raw output into PhEval standardised TSV output
         print("post processing results to PhEval standardised TSV output.")
